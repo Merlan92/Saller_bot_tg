26413e0f87534e73530cf5e95b618643f3e1e6f5
Merlan92 <163337013+Merlan92@users.noreply.github.com>
2025-07-09 14:05:14 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,2 @@
-API_TOKEN = '7794647410:AAG9d2mOedbSLg8Hz2T3v2AgpKXqy8SFFZI'
-DB_URL = 'postgresql://neondb_owner:npg_zSOewByG8a1j@ep-round-silence-a9a0ls8t-pooler.gwc.azure.neon.tech/neondb?sslmode=require&channel_binding=require'
-SELLER_PASSWORD = "seller222"
-DIRECTOR_PASSWORD = "director222"
+ фцупупывп
+
